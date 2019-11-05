ec0c49ecbdca18adbb7387cddbf34d9834b771d1
harshthakkar-code <harryparkar111@gmail.com>
2019-11-05 12:00:06 +0530

    view more data complete

--- src/app/customer/view-more/view-more.component.ts
+++ src/app/customer/view-more/view-more.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Router } from '@angular/router';
+import { CustDataService } from '../cust-data.service';
+import { CustTbl } from '../cust-tbl';
 
 @Component({
   selector: 'app-view-more',
@@ -8,11 +11,14 @@ import { MatDialogRef } from '@angular/material';
 })
 export class ViewMoreComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ViewMoreComponent>) { }
+  constructor(public dialogRef: MatDialogRef<ViewMoreComponent>,private _data:CustDataService, @Inject(MAT_DIALOG_DATA) private row:CustTbl) { }
   onCancelClick() {
     this.dialogRef.close();
   }
+  data:CustTbl;
   ngOnInit() {
+    this.data=this.row;
+      }
   }
 
-}
+
